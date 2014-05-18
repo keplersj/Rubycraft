d815851ff59a61ee6a3dcc5a65d1f19ea3e33e3b
Kepler Sticka-Jones <kbsj269@gmail.com>
2014-05-18 00:39:06 -0600

    Hey look! Event handling!

--- src/main/ruby/Rubycraft.rb
+++ src/main/ruby/Rubycraft.rb
@@ -3,7 +3,25 @@ require 'java'
 java_package 'k2b6s9j.Rubycraft'
 
 java_import 'cpw.mods.fml.common.Mod'
+java_import 'cpw.mods.fml.common.Mod.EventHandler'
+java_import 'cpw.mods.fml.common.event.FMLPreInitializationEvent'
+java_import 'cpw.mods.fml.common.event.FMLInitializationEvent'
+java_import 'cpw.mods.fml.common.event.FMLPostInitializationEvent'
 
 java_annotation 'Mod(modid = "Rubycraft", name = "Rubycraft", version = "0.1")'
 class Rubycraft
+  java_annotation 'EventHandler'
+  java_signature 'void preInit(FMLPreInitializationEvent)'
+  def preInit(event)
+  end
+
+  java_annotation 'EventHandler'
+  java_signature 'void init(FMLInitializationEvent)'
+  def init(event)
+  end
+
+  java_annotation 'EventHandler'
+  java_signature 'void postInit(FMLPostInitializationEvent)'
+  def postInit(event)
+  end
 end
