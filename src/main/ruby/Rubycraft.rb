3a8d16f2978fd913dc49b1317357fab74693096b
Kepler Sticka-Jones <kbsj269@gmail.com>
2014-05-17 21:54:48 -0600

    This should produce a Java class with a FML Mod annotation.

--- src/main/ruby/Rubycraft.rb
+++ src/main/ruby/Rubycraft.rb
@@ -1,4 +1,9 @@
 require 'java'
 
+java_package 'k2b6s9j.Rubycraft'
+
+java_import 'cpw.mods.fml.common.Mod'
+
+java_annotation 'Mod(modid = "Rubycraft", name = "Rubycraft", version = "0.1")'
 class Rubycraft
 end
